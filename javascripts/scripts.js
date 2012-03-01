b2978f6cdca87197d573dba28018135df5bff961
Ryan Schenk <rschenk@gmail.com>
2012-03-01 16:19:07 -0500

    Tweaked the genus autocompleter to use ids, but then changed back to labels

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,32 +1,6 @@
 var ZooBank = {
 	config: { endpoint: 'http://test.zoobank.org/editor/services.cfc' },
 	
-	autocompleteGenus: function(selector) {
-		$(selector).customautocomplete({
-	        source: function(request, response) {
-				$.ajax({
-					url: ZooBank.config.endpoint + "?method=get_protonym&RankGroup=Genus&display_type=autocomplete",
-					type: 'post',
-					dataType: 'json',
-					data: {term: request.term},
-					success: function(data) {
-						if( data[0].id === 0 ) {
-							response(null);
-						} else {
-							response($.map(data, function(d, i){ 
-								d.value = d.label; // The value has HTML in it, which never works anyways
-								return d;
-							}));
-						}
-					}
-				})
-	        },
-	        minLength: 3,
-			open: function(){},
-			noresults: function(event,searchTerm){}
-	      });
-	},
-	
 	autocompleteAuthor: function(selector) {
 		$(selector).customautocomplete({
 	        source: function(request, response) {
