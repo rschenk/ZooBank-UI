00936e4585e534e3f8565f3ba66af2630c103dc8
Ryan Schenk <rschenk@gmail.com>
2012-03-14 15:27:07 -0400

    Updated Reference to build DatePublished as a string

--- javascripts/app/models/reference.js
+++ javascripts/app/models/reference.js
@@ -36,6 +36,30 @@ var Reference = Backbone.Model.extend({
 		this.set({referenceTypeId: this.get('referenceTypeId') || this.constructor.JOURNAL_ARTICLE });
 	},
 	
+	/* 
+	 * Builds a string according to the w3c date recommendations http://www.w3.org/TR/NOTE-datetime
+	 * 
+	 * Year:
+	 * 	YYYY (eg 1997)
+	 * Year and month:
+	 * 	YYYY-MM (eg 1997-07)
+	 * Complete date:
+	 * 	YYYY-MM-DD (eg 1997-07-16)
+	 */
+	datePublished: function(){
+		var datePublished = this.get('year');
+		
+		if( this.get('month') ) {
+			datePublished += "-" + this.get('month');
+			
+			if( this.get('day') ) {
+				datePublished += "-" + this.get('day');
+			}
+		}
+		
+		return datePublished;
+	},
+	
 	/* Return a copy of the model's attributes for JSON stringification. 
 	 * This is used to prepare the object for persisting to the web service.
 	 * It doesn't actually return a JSON string — but that's the way the JavaScript API for JSON.stringify works.
@@ -44,7 +68,9 @@ var Reference = Backbone.Model.extend({
 		var jsonAttributes = _(this.attributes).clone();
 		
 		return _(jsonAttributes).extend({
-			authors: this.get('authors').toJSON() // Why Backbone doesn't call toJSON by default is beyond me!
+			Authors: this.get('authors').toJSON(), // Why Backbone doesn't call toJSON by default is beyond me!
+			DatePublished: this.datePublished(),
+			LogUserName: 'true'
 		});
 	}
 },{
